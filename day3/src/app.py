bbcc1fa95df8b342dbda50a9b022684140d202f1
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-01-16 17:41:13 +0530

    day3 docker-compose fix

--- day3/src/app.py
+++ day3/src/app.py
@@ -5,9 +5,9 @@ from datetime import datetime, timezone
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/postgres'   # running locally
 # postgres_link = 'postgresql://{user}:{password}@{host}:{port}/{database}'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/mydb'   # with docker-compose
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/mydb'   # with docker-compose
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password123@flask-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password123@flask-db.cfykukwcw419.ap-south-1.rds.amazonaws.com:5432/mydb'
 
 # Initialize SQLAlchemy with app context
 db = SQLAlchemy()
@@ -53,4 +53,4 @@ def mark_attendance():
 
 if __name__ == '__main__':
     init_db()  # Initialize database tables before running the app
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8000)
