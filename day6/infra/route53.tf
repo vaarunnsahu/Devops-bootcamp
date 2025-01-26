972b8f22b79d858cc430752b1778bd3a03d00dae
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-01-26 22:43:50 +0530

    atst

--- day6/infra/route53.tf
+++ day6/infra/route53.tf
@@ -16,7 +16,7 @@ resource "aws_route53_record" "app" {
 
   alias {
     name                   = aws_lb.alb.dns_name
-    zone_id                = aws_lb.alb.zone_id
+    zone_id                  = aws_lb.alb.zone_id
     evaluate_target_health = true
   }
 }
