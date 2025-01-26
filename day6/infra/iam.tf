4f3a71ee7ca4b6486a205fb950c07aa8aa7e1ead
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-01-26 23:01:21 +0530

    checkov skip

--- day6/infra/iam.tf
+++ day6/infra/iam.tf
@@ -53,6 +53,7 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
+  #checkov:skip=CKV_AWS_290:we are implementing it soon
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
