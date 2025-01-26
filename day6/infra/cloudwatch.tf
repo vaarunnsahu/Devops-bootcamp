9a9e35a0e99db7ea54550ff0dbd2d43fa85cbf9f
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2025-01-26 23:02:21 +0530

    checkov skip

--- day6/infra/cloudwatch.tf
+++ day6/infra/cloudwatch.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_group" "ecs" {
   name              = "/aws/ecs/${var.environment}-${var.app_name}"
   retention_in_days = 30
-  kms_key_id      = aws_kms_key.rds_kms.arn
+  kms_key_id        = aws_kms_key.rds_kms.arn
 }
 
 resource "aws_cloudwatch_query_definition" "ecs" {
