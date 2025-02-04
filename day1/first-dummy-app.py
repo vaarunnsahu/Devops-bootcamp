108c200e2c8da62f2b0f0c8a8af54699e2656096
vaarunnsahu <varundepanjalisahu@gmail.com>
2025-02-04 15:04:26 +0530

    update

--- day1/first-dummy-app.py
+++ day1/first-dummy-app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> This is Akhilesh Mishra</h1><br><p>Thank you for joining.. I hope you enjoyed it, follow for more content around Devops.</p> '
+    return '<h1> This is Varun Sahu</h1><br><p>https://www.linkedin.com/in/vaarunsahu/, follow for more content around Devops.</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
